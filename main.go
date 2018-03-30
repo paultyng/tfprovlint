6e008faa8df960f8aa49ac08fc1eac02bc2b238b
Paul Tyng <paul@paultyng.net>
2018-03-30 13:53:55 -0400

    Add main entrypoint

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/paultyng/tfprovlint/provparse"
+)
+
 func main() {
 	path := "/home/paul/go/src/github.com/terraform-providers/terraform-provider-azurerm/azurerm"
 
+	_, err := provparse.Package(path)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
